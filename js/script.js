dae7621d77f150675965b0793995f0184bbd6415
Bailey Perillo <13bailey11@gmail.com>
2018-11-19 23:12:18 +0000

    rps draft 2-conditionals/random

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var userChoice = "";
+var computerChoice = "paper";
+var winner = "";
+var randomNumber = Math.random();
+
+
+$("#shoot").click(function() {
+    var input = $("#input").val();
+    $(userChoice).text(input);
+    
+    if (userChoice == "rock" && computerChoice == "paper") {
+        $(winner).text("computer wins");
+    }else { 
+        $(".results").text("user wins");
+    }
+});
+
+
 
 
 
