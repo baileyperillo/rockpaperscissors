567729eb18186039162df8bc9fce19567a01978f
Bailey Perillo <13bailey11@gmail.com>
2018-11-26 23:13:13 +0000

    draft3-compund conditionals

--- js/script.js
+++ js/script.js
@@ -5,20 +5,37 @@
 //GLOBAL VARIABLES
 /* global $ */
 var userChoice = "";
-var computerChoice = "paper";
+var computerChoice = "";
 var winner = "";
 var randomNumber = Math.random();
 
 
 $("#shoot").click(function() {
     var input = $("#input").val();
-    $(userChoice).text(input);
+    $("#userChoice").text(input);
+    var userChoice = input;
     
-    if (userChoice == "rock" && computerChoice == "paper") {
-        $(winner).text("computer wins");
-    }else { 
-        $(".results").text("user wins");
+    if(randomNumber > .75) {
+        $("#computerChoice").text("rock");
+        var computerChoice = "rock";
+    }else if(randomNumber <.75 && randomNumber >.50) {
+        $("#computerChoice").text("paper");
+        var computerChoice = "paper";
+    }else{
+        $("#computerChoice").text("scissors");
+        var computerChoice = "scissors";
     }
+    
+ if (userChoice ==="rock" && computerChoice === "paper"||userChoice ==="paper" && computerChoice==="scissors"|| userChoice ==="scissors" && computerChoice ==="rock") {
+     var winner = "computer wins!"
+     $("#result").text(winner);
+ }else if(computerChoice ==="rock" && userChoice === "paper"||computerChoice ==="paper" && userChoice==="scissors"|| computerChoice ==="scissors" && userChoice ==="rock") {
+    var winner = "user wins!";
+     $("#result").text(winner);
+ }else {
+     $("#result").text("not valid input");
+ }
+ 
 });
 
 
